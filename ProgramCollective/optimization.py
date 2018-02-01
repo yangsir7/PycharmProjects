70c534c93366bbf447c499f1e6d83b89d2ba831d
yangsir7 <yangsir7@github.com>
2018-02-01 16:20:45 +0800

    2018.1.28 Click-Tracking optimization

--- ProgramCollective/optimization.py
+++ ProgramCollective/optimization.py
@@ -29,17 +29,21 @@ def schedulecost(sol):
     latestarrival=0
     earliestdep=24*60
     # ll=int(len(sol) / 2)
-    for d in range(int(len(sol) / 2)):
-        # Get the inbound and outbound flights
-        origin=people[d][1]
-        outbound=flights[(origin,destination)][int(sol[d])]
-        returnf=flights[(destination,origin)][int(sol[d+1])]
-        # Total price is the price of all outbound and return flights
-        totalprice+=outbound[2]
-        totalprice+=returnf[2]
-        # Track the latest arrival and earliest departure
-        if latestarrival<getminutes(outbound[1]): latestarrival=getminutes(outbound[1])
-        if earliestdep>getminutes(returnf[0]): earliestdep=getminutes(returnf[0])
+    try:
+        for d in range(int(len(sol)/2)):
+            # Get the inbound and outbound flights
+            origin=people[d][1]
+            outbound=flights[(origin,destination)][int(sol[d])]
+            returnf=flights[(destination,origin)][int(sol[d+1])]
+            # Total price is the price of all outbound and return flights
+            totalprice+=outbound[2]
+            totalprice+=returnf[2]
+            # Track the latest arrival and earliest departure
+            if latestarrival<getminutes(outbound[1]): latestarrival=getminutes(outbound[1])
+            if earliestdep>getminutes(returnf[0]): earliestdep=getminutes(returnf[0])
+    except:
+        print(sol)
+        print(len(sol))
     # Every person must wait at the airport until the latest person arrives.
     # They also must arrive at the same time and wait for their flights.
     totalwait=0
@@ -116,3 +120,54 @@ def annealingoptimize(domain,costf,T=10000.0,cool=0.95,step=1):
         # Decrease the temperature
         T=T*cool
     return vec
+def geneticoptimize(domain,costf,popsize=50,step=1,mutprod=0.2,elite=0.2,maxiter=100):
+    # Mutation Operation
+    def mutate(vec):
+        i=random.randint(0,len(domain)-1)
+        if random.random()<0.5 and vec[i]>domain[i][0]:
+            return vec[0:i]+[vec[i]-step]+vec[i+1:]
+        elif vec[i]<domain[i][1]:
+            return vec[0:i]+[vec[i]+step]+vec[i+1:]
+        else:
+            return vec
+    # Crossover Operation
+    def crossover(r1,r2):
+        i=random.randint(1,len(domain)-2)
+        return r1[0:i]+r2[i:]
+    # Build the initial population
+    pop=[]
+    for i in range(popsize):
+        vec=[random.randint(domain[i][0],domain[i][1]) for i in range(len(domain))]
+        pop.append(vec)
+    # How many winners from each generation?
+    topelite=int(elite*popsize)
+    # Main loop
+    for i in range(maxiter):
+        scores=[(costf(v),v) for v in pop]
+        # scores=[]
+        # k=0
+        # for v in pop:
+        #     k+=1
+        #     if v!=None:scores.append([costf(v),v])
+        #     else:
+        #         print(k)
+        #         print(pop)
+        scores.sort()
+        ranked=[v for (s,v) in scores]
+        # Start with the pure winners
+        pop=ranked[0:topelite]
+        ranked=pop
+        # Add mutated and bred forms of the winners
+        while len(pop)<popsize:
+            if random.random()<mutprod:
+                # Mutation
+                c = random.randint(0, topelite-1)
+                pop.append(mutate(ranked[c]))
+            else:
+                # Crossover
+                c1 = random.randint(0, topelite-1)
+                c2 = random.randint(0, topelite-1)
+                pop.append(crossover(ranked[c1], ranked[c2]))
+        # Print current best score
+        print(scores[0][0])
+    return scores[0][1]
--- ProgramCollective/test3.py
+++ ProgramCollective/test3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #-*- coding=cp936 -*-
-
+import time
+import random
+import math
 import urllib.request
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
@@ -16,13 +18,27 @@ import time
 # print([r for r in seg_list])
 # print(stopwords)
 # s=[1,4,3,2,7,3,6,3,2,4,5,3]
-domain=[(0,8)]*(len(optimization.people)*2)
+domain=[(0,9)]*(len(optimization.people)*2)
 # s=optimization.randomoptimize(domain,optimization.schedulecost)
 # sc=optimization.schedulecost(s)
 # s=optimization.hillclimb(domain,optimization.schedulecost)
 # sc=optimization.schedulecost(s)
-s=optimization.annealingoptimize(domain,optimization.schedulecost)
-sc=optimization.schedulecost(s)
-optimization.printschedule(s)
-print(s)
-print(sc)
+# s=optimization.annealingoptimize(domain,optimization.schedulecost)
+# sc=optimization.schedulecost(s)
+# s=optimization.geneticoptimize(domain,optimization.schedulecost)
+# optimization.printschedule(s)
+# sc=optimization.schedulecost(s)
+# print(s)
+# print(sc)
+
+from ProgramCollective import dorm
+dorm.printsolution([0,0,0,0,0,0,0,0,0,0])
+s=optimization.randomoptimize(dorm.domain,dorm.dormcost)
+dorm.dormcost(s)
+optimization.geneticoptimize(dorm.domain,dorm.dormcost)
+dorm.printsolution(s)
+# pop=[]
+# for i in range(50):
+#     vec=[random.randint(0,9) for i in range(len(domain))]
+#     print(i,'\t',vec)
+#     pop.append(vec)
