fbb030b52fbf3ad8ea0bcb984cbc701937279496
yangsir7 <yangsir7@github.com>
2018-02-01 10:02:05 +0800

    2018.1.28 Click-Tracking optimization

--- ProgramCollective/optimization.py
+++ ProgramCollective/optimization.py
@@ -7,7 +7,7 @@ destination='LGA'
 
 flights={}
 #
-for line in open('schedule.txt'):
+for line in open('C:\\Users\\yangquan1\\PycharmProjects\\ProgramCollective\\schedule.txt'):
     origin,dest,depart,arrive,price=line.strip( ).split(',')
     flights.setdefault((origin,dest),[])
     # Add details to the list of possible flights
@@ -17,18 +17,19 @@ def getminutes(t):
     return x[3]*60+x[4]
 
 def printschedule(r):
-    for d in range(len(r)/2):
+    for d in range(int(len(r)/2)):
         name=people[d][0]
         origin=people[d][1]
-        out=flights[(origin,destination)][r[d]]
-        ret=flights[(destination,origin)][r[d+1]]
+        out=flights[(origin,destination)][int(r[d])]
+        ret=flights[(destination,origin)][int(r[d+1])]
         print('%10s%10s %5s-%5s $%3s %5s-%5s $%3s' % (name,origin,out[0],out[1],out[2],ret[0],ret[1],ret[2]))
 
 def schedulecost(sol):
     totalprice=0
     latestarrival=0
     earliestdep=24*60
-    for d in range(len(sol)/2):
+    # ll=int(len(sol) / 2)
+    for d in range(int(len(sol) / 2)):
         # Get the inbound and outbound flights
         origin=people[d][1]
         outbound=flights[(origin,destination)][int(sol[d])]
@@ -42,14 +43,14 @@ def schedulecost(sol):
     # Every person must wait at the airport until the latest person arrives.
     # They also must arrive at the same time and wait for their flights.
     totalwait=0
-    for d in range(len(sol)/2):
+    for d in range(int(len(sol)/2)):
         origin=people[d][1]
         outbound=flights[(origin,destination)][int(sol[d])]
         returnf=flights[(destination,origin)][int(sol[d+1])]
         totalwait+=latestarrival-getminutes(outbound[1])
         totalwait+=getminutes(returnf[0])-earliestdep
     # Does this solution require an extra day of car rental? That'll be $50!
-    if latestarrival>earliestdep: totalprice+=50
+    if latestarrival<earliestdep: totalprice+=50
     return totalprice+totalwait
 
 def randomoptimize(domain,costf):
@@ -64,7 +65,7 @@ def randomoptimize(domain,costf):
         if cost<best:
             best=cost
             bestr=r
-    return r
+    return bestr
 
 def hillclimb(domain,costf):
     # Create a random solution
@@ -91,3 +92,27 @@ def hillclimb(domain,costf):
         if best==current:
             break
     return sol
+def annealingoptimize(domain,costf,T=10000.0,cool=0.95,step=1):
+    # Initialize the values randomly
+    vec=[float(random.randint(domain[i][0],domain[i][1])) for i in range(len(domain))]
+    while T>0.1:
+        # Choose one of the indices
+        i=random.randint(0,len(domain)-1)
+        # Choose a direction to change it
+        dir=random.randint(-step,step)
+        # Create a new list with one of the values changed
+        vecb=vec[:]
+        vecb[i]+=dir
+        if vecb[i]<domain[i][0]: vecb[i]=domain[i][0]
+        elif vecb[i]>domain[i][1]: vecb[i]=domain[i][1]
+        # Calculate the current cost and the new cost
+        ea=costf(vec)
+        eb=costf(vecb)
+        p=pow(math.e,(-eb-ea)/T)
+        # Is it better, or does it make the probability
+        # cutoff?
+        if (eb<ea or random.random( )<p):
+            vec=vecb
+        # Decrease the temperature
+        T=T*cool
+    return vec
--- ProgramCollective/test3.py
+++ ProgramCollective/test3.py
@@ -7,6 +7,7 @@ from urllib.parse import urljoin
 from sqlite3 import dbapi2 as sqlite
 import re
 from ProgramCollective import nn
+from ProgramCollective import optimization
 import jieba
 import time
 # jieba.add_word('路明非')
@@ -14,9 +15,14 @@ import time
 # seg_list = jieba.cut("我来到北京清华大学", cut_all=False)
 # print([r for r in seg_list])
 # print(stopwords)
-def getminutes(t):
-    x=time.strptime(t,'%H:%M')
-    return x[3]*60+x[4]
-# print(time.clock())
-a=getminutes('01:00')
-print(a)
+# s=[1,4,3,2,7,3,6,3,2,4,5,3]
+domain=[(0,8)]*(len(optimization.people)*2)
+# s=optimization.randomoptimize(domain,optimization.schedulecost)
+# sc=optimization.schedulecost(s)
+# s=optimization.hillclimb(domain,optimization.schedulecost)
+# sc=optimization.schedulecost(s)
+s=optimization.annealingoptimize(domain,optimization.schedulecost)
+sc=optimization.schedulecost(s)
+optimization.printschedule(s)
+print(s)
+print(sc)
